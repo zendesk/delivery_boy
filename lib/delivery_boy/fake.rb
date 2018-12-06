af2061d45dd13868ad79a898dd4bb336e4c95098
Ashu Pachauri <ashu210890@gmail.com>
2018-12-06 23:32:41 +0530

    Thread-safe message delivery in testing mode

--- lib/delivery_boy/fake.rb
+++ lib/delivery_boy/fake.rb
@@ -10,13 +10,16 @@ module DeliveryBoy
 
     def initialize
       @messages = Hash.new {|h, k| h[k] = [] }
+      @delivery_lock = Mutex.new
     end
 
     def deliver(value, topic:, key: nil, partition: nil, partition_key: nil, create_time: Time.now)
-      offset = @messages[topic].count
-      message = FakeMessage.new(value, topic, key, offset, partition, partition_key, create_time)
+      @delivery_lock.synchronize do
+        offset = @messages[topic].count
+        message = FakeMessage.new(value, topic, key, offset, partition, partition_key, create_time)
 
-      @messages[topic] << message
+        @messages[topic] << message
+      end
 
       nil
     end
@@ -29,12 +32,18 @@ module DeliveryBoy
 
     # Clear all messages stored in memory.
     def clear
-      @messages.clear
+      @delivery_lock.synchronize do
+        @messages.clear
+      end
     end
 
     # Return all messages written to the specified topic.
     def messages_for(topic)
-      @messages[topic]
+      @delivery_lock.synchronize do
+        # Return a clone so that the list of messages can be traversed
+        # without worrying about a concurrent modification
+        @messages[topic].clone
+      end
     end
   end
 end
