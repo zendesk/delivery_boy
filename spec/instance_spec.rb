5bb499606eb7eb7d4f526472258efb2c37183db7
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2019-01-21 13:45:11 +0100

    Create the topic first

--- spec/instance_spec.rb
+++ spec/instance_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe DeliveryBoy::Instance do
   let(:config) { DeliveryBoy::Config.new }
   let(:instance) { DeliveryBoy::Instance.new(config, logger) }
 
+  before :all do
+    kafka = Kafka.new(DeliveryBoy.config.brokers, logger: DeliveryBoy.logger)
+    begin
+      kafka.create_topic("greetings")
+    rescue => e
+      DeliveryBoy.logger.warn "Topic `greetings` already created"
+    end
+  end
+
   describe "#deliver" do
     it "delivers a message to Kafka" do
       instance.deliver("hello", topic: "greetings")
