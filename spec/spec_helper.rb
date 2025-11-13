ccef2d496b61990c230606e43f9f25b9ee90b1a8
Benjamin Quorning <benjamin@quorning.net>
2025-11-13 17:09:17 +0100

    docker-api healthcheck doesn't work on CI?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,16 +7,9 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     puts "Starting test containers"
-    config.container = Testcontainers::KafkaContainer.new(
-      healthcheck: {
-        test: ["kafka-broker-api-versions", "--bootstrap-server", "localhost:9092"],
-        interval: 0.2,
-        timeout: 1,
-        retries: 20
-      }
-    )
+    config.container = Testcontainers::KafkaContainer.new
     config.container.start
-    config.container.wait_for_healthcheck
+    kafka_container_health_check(config.container)
   end
 
   config.after(:suite) do
@@ -34,5 +27,25 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+end
+
+def kafka_container_health_check(container)
+  command = %w[kafka-broker-api-versions --bootstrap-server localhost:9092]
+
+  max_time = 10 # seconds
+  end_time = Time.now + max_time
+
+  loop do
+    _stdout, _stderr, exit_code = container._container.exec(command)
+
+    if exit_code != 0
+      raise "Kafka is not ready" if Time.now > end_time
+      print "·"
+      sleep 0.5
+    else
+      puts
+      return true
+    end
   end
 end
