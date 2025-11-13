201e7d47c67476c974399cf885c416c90ce2cd15
Benjamin Quorning <benjamin@quorning.net>
2025-11-13 15:34:09 +0100

    Use testcontainers for testing, locally and on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,31 @@
 require "bundler/setup"
 require "delivery_boy"
+require "testcontainers/kafka"
 
 RSpec.configure do |config|
+  config.add_setting :container, default: nil
+
+  config.before(:suite) do
+    puts "Starting test containers"
+    config.container = Testcontainers::KafkaContainer.new(
+      healthcheck: {
+        test: ["kafka-broker-api-versions", "--bootstrap-server", "localhost:9092"],
+        interval: 0.2,
+        timeout: 1,
+        retries: 20
+      }
+    )
+    config.container.start
+    config.container.wait_for_healthcheck
+  end
+
+  config.after(:suite) do
+    puts
+    puts "Shutting down test containers"
+    config.container.stop if config.container&.running?
+    config.container.remove if config.container&.exists?
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
